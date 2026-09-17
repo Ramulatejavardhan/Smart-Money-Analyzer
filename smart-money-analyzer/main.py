0ef0106a454aa8b1e8d15cb3079b218e9aad6b47
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-09-18 01:37:56 +0530

    intial

--- smart-money-analyzer/main.py
+++ smart-money-analyzer/main.py
@@ -12,23 +12,23 @@ while true:
     print("8. Spending analysis")
     print("9. Exit")
     choice=input("Enter ur choice:")
-if choice =="1":
-    print("Add transaction")
-elif choice =="2":
-    print("View transactions")
-elif choice =="3":
-    print("Delete transaction")
-elif choice =="4":
-    print("Search and filter")
-elif choice =="5":
-    print("View summary")
-elif choice =="6":
-    print("Manage budget")
-elif choice =="7":
-    print("Monthly report")
-elif choice =="8":
-    print("Spending analysis")
-elif choice =="9":
-    print("See u again bye u selected EXIT choice")
-else:
-    print("select a valid choice")
+    if choice =="1":
+        print("Add transaction")
+    elif choice =="2":
+        print("View transactions")
+    elif choice =="3":
+        print("Delete transaction")
+    elif choice =="4":
+        print("Search and filter")
+    elif choice =="5":
+        print("View summary")
+    elif choice =="6":
+        print("Manage budget")
+    elif choice =="7":
+        print("Monthly report")
+    elif choice =="8":
+        print("Spending analysis")
+    elif choice =="9":
+        print("See u again bye u selected EXIT choice")
+    else:
+        print("select a valid choice")
