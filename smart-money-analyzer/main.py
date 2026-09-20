ab808a025c80a8f12047df9911ec6fb4cbd1cbe2
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-09-20 19:32:53 +0530

    loopimplementation

--- smart-money-analyzer/main.py
+++ smart-money-analyzer/main.py
@@ -1,35 +1,23 @@
 print("================================")
-print("     SMART MONEY ANALYZER")
+print("       SMART MONEY ANALYZER")
 print("================================")
+
 while True:
- print("1. Add transaction")
- print("2. View transactions")
- print("3. Delete transaction")
- print("4. Search and filter")
- print("5. View summary")
- print("6. Manage budget")
- print("7. Monthly report")
- print("8. Spending analysis")
- print("9. Exit")
- choice=input("Enter ur choice:")
- if choice =="1":
-     print("Add transaction")
- elif choice =="2":
-     print("View transactions")
- elif choice =="3":
-     print("Delete transaction")
- elif choice =="4":
-     print("Search and filter")
- elif choice =="5":
-     print("View summary")
- elif choice =="6":
-     print("Manage budget")
- elif choice =="7":
-     print("Monthly report")
- elif choice =="8":
-     print("Spending analysis")
- elif choice =="9":
-     print("See u again bye u selected EXIT choice")
-     break
- else:
-     print("select a valid choice")
+
+    print("1. Add transaction")
+    print("2. View transactions")
+    print("3. Delete transaction")
+    print("4. Search and filter")
+    print("5. View summary")
+    print("6. Manage budget")
+    print("7. Monthly report")
+    print("8. Spending analysis")
+    print("9. Exit")
+
+    choice = input("Enter ur choice: ")
+
+    print("You entered:", choice)
+
+    if choice == "9":
+        print("Goodbye!")
+        break
