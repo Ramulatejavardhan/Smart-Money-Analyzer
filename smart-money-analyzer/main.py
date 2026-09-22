828f527febbdc7c1978f8bb29b6277bca0743f39
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-09-22 14:37:08 +0530

    transaction

--- smart-money-analyzer/main.py
+++ smart-money-analyzer/main.py
@@ -1,11 +1,8 @@
 import json
-
 print("================================")
 print("       SMART MONEY ANALYZER")
 print("================================")
-
 while True:
-
     print("1. Add transaction")
     print("2. View transactions")
     print("3. Delete transaction")
